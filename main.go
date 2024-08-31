5a7ce92964d4ea2fa5a80c5d8b576cda2061fe9f
LoydNgei <loydngei@gmail.com>
2024-08-31 22:42:14 +0300

    adding more functions

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
-import ("fmt")
+import "fmt"
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets here to attend\n")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
 
-const ticketNumber = 20
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
 
 
-Printf("There are %v tickets remaining" ticketNumber)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName,userTickets, email)
+
+	fmt.Printf("%v tickets remaining for %v\n",remainingTickets, conferenceName )
+}
