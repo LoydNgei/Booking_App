3b4585acd3e8b5a9881815addbbe7bbe55afec73
LoydNgei <loydngei@gmail.com>
2024-08-31 23:14:35 +0300

    Slices in Go and implementation

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-
+	var bookings []string
+ 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your tickets here to attend\n")
@@ -30,9 +31,13 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName + " " + lastName)
+
 
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName,userTickets, email)
 
 	fmt.Printf("%v tickets remaining for %v\n",remainingTickets, conferenceName )
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 }
